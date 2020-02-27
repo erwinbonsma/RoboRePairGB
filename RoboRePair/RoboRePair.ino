031abefed79b1f9ec8d81dff68ae315290b4a26c
Erwin Bonsma <eriban@users.sourceforge.net>
2020-02-27 15:35:23 +0100

    Improve CPU indicator
    Made it less intrusive.
    Also, let it display properly when screen is not (fully) cleared.

--- RoboRePair/RoboRePair.ino
+++ RoboRePair/RoboRePair.ino
@@ -7,10 +7,23 @@ UpdateFunction updateFunction;
 DrawFunction drawFunction;
 
 void displayCpuLoad() {
-  uint8_t cpuLoad = gb.getCpuLoad();
-  gb.display.setColor(cpuLoad < 80 ? INDEX_GREEN : (cpuLoad < 100 ? INDEX_YELLOW : INDEX_RED));
-  gb.display.setCursor(100, 1);
-  gb.display.printf("%d", cpuLoad);
+  int cpuLoad = gb.getCpuLoad();
+  if (cpuLoad >= 70) {
+    gb.display.setColor(INDEX_GREEN);
+    gb.display.drawLine(0, 127, min(9, cpuLoad - 70), 127);
+  }
+  if (cpuLoad >= 80) {
+    gb.display.setColor(INDEX_YELLOW);
+    gb.display.drawLine(10, 127, 10 + min(20, cpuLoad - 80), 127);
+  }
+  if (cpuLoad >= 100) {
+    gb.display.setColor(INDEX_RED);
+    gb.display.drawLine(30, 127, 30 + min(20, cpuLoad - 100), 127);
+  }
+  if (cpuLoad < 120) {
+    gb.display.setColor(INDEX_BLACK);
+    gb.display.drawLine(max(0, cpuLoad - 69), 127, 50, 127);
+  }
 }
 
 void setup() {
