e5740ead330d28da4657848e3b168f39ccd4c630
Erwin Bonsma <eriban@users.sourceforge.net>
2020-03-15 13:49:04 +0100

    Increase sound buffer size

--- RoboRePair/config-gamebuino.h
+++ RoboRePair/config-gamebuino.h
@@ -1,1 +1,10 @@
 #define DISPLAY_MODE DISPLAY_MODE_INDEX
+
+#define FOLDER_NAME "BumbleBotsRePair"
+
+// Double sound buffer size so music can play more smoothly.
+#define SOUND_BUFFERSIZE 4096
+
+// Reduce number of channels. Only two should be needed, one for FX, another for music.
+// Note FX has a dedicated channel, so one channel suffices.
+#define SOUND_CHANNELS 2
